2ef0537203c4ede7b573300a61557e429d67ff94
Node Central <chris_parker_uk@hotmail.co.uk>
2022-09-21 08:59:01 +0100

    Update and rename whats_my_external_ip.lua to external_ip.lua

--- external_ip.lua
+++ external_ip.lua
@@ -1,6 +1,3 @@
---
-
-require "xxprowlnotification" 
 local http = require("socket.http")
 local json = require("dkjson")
 
@@ -8,6 +5,7 @@ local json = require("dkjson")
 -- Fetches the external IP.
 -- Uses http://ipinfo.io web service.
 -- @return string IP address, or `nil + errormsg`
+-----------------------------------------
 
 local function externalip()
 	local resp,code,headers,status = http.request("http://ipinfo.io/json")
@@ -19,10 +17,9 @@ local function externalip()
 			return nil, "Failed fetching external IP; "..tostring(err)
 		end
 		if data.ip then
-			xxprowlnotification.my_prowl ("VeraPlus Cabin", "My External IP Notification", "Your External IP is " .. data.ip )
 			return data.ip
   		else
-    		return nil, "Failed fetching external IP; no ip field was returned"
+    			return nil, "Failed fetching external IP; no ip field was returned"
   		end
 	end
 
